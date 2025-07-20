229b3c5be0350c069fb77e94ac82aedbf91b761c
Sushant Jarial <sushantjarial7@gmail.com>
2025-07-20 22:30:53 +0530

    fixed

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,25 +1,17 @@
-import Image from "next/image";
-import { FloatingDock } from "@/components/ui/floating-dock";
-import {
-  IconBrandGithub,
-  IconBrandX,
-  IconExchange,
-  IconHome,
-  IconNewSection,
-  IconTerminal2,
-} from "@tabler/icons-react";
 import Toolbar from "@/components/ui/Toolbar";
 import Board from "@/components/Board";
 export default function Home() {
   return (
-  <div>
     <div>
-      <div className="absolute"><Board /></div>
-      <div className=" z-50" > <Toolbar /></div>
-      
+      <div>
+        <div className="absolute">
+          <Board />
+        </div>
+        <div className=" z-50">
+          {" "}
+          <Toolbar />
+        </div>
+      </div>
     </div>
- 
-   
-  </div>
   );
 }
